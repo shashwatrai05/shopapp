f6dcaf7155bd19e6874bf332b5cdc2e272ae4584
ShashwatRai575 <shashwatrai575@gmail.com>
2023-06-14 00:58:40 +0530

    Tuesday

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopapp/providers/auth.dart';
 import 'package:shopapp/providers/orders.dart';
 import 'package:shopapp/screen/cart_screen.dart';
 import 'package:shopapp/screen/orders_screen.dart';
@@ -9,36 +10,39 @@ import 'package:provider/provider.dart';
 import './providers/cart.dart';
 import './screen/user_products_screen.dart';
 import './screen/edit_product_screen.dart';
+import './screen/auth_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(providers: [
-ChangeNotifierProvider(
-      create:(ctx)=> Products(),),
-ChangeNotifierProvider(
-  create:(ctx)=> Cart()),
-ChangeNotifierProvider(
-  create: (ctx)=>Orders())
-
-    ],
-      child: MaterialApp(
-        title: 'MyShop',
-        theme: ThemeData(
-          fontFamily: 'Lato', colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple).copyWith(secondary: Colors.deepOrange),
-        ),
-        home: ProductOverviewScreen(),
-        routes: {
-          ProductDetailScreen.routeName:(ctx) => ProductDetailScreen(),
-          CartScreen.routeName:(ctx)=> CartScreen(),
-          OrdersScreen.routeName:(ctx)=>OrdersScreen(),
-          UserProductsScreen.routeName:(ctx)=>UserProductsScreen(),
-          EditProductScreen.routeName:(ctx)=>EditProductScreen(),
-        },
-      ),
-    );
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (ctx) => Auth()),
+          ChangeNotifierProvider(
+            create: (ctx) => Products(),
+          ),
+          ChangeNotifierProvider(create: (ctx) => Cart()),
+          ChangeNotifierProvider(create: (ctx) => Orders())
+        ],
+        child: Consumer<Auth>(
+          builder: (ctx, auth, _) => MaterialApp(
+            title: 'MyShop',
+            theme: ThemeData(
+              fontFamily: 'Lato',
+              colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
+                  .copyWith(secondary: Colors.deepOrange),
+            ),
+            home: auth.isAuth ? ProductOverviewScreen() : AuthScreen(),
+            routes: {
+              ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+              CartScreen.routeName: (ctx) => CartScreen(),
+              OrdersScreen.routeName: (ctx) => OrdersScreen(),
+              UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
+              EditProductScreen.routeName: (ctx) => EditProductScreen(),
+            },
+          ),
+        ));
   }
 }
- 
---
+++ lib/providers/auth.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import '../models/http_exceptions.dart';
+
+class Auth with ChangeNotifier {
+  String _token;
+  DateTime _expirydate;
+  String _userId;
+
+  bool get isAuth {
+    return token != null;
+  }
+
+  String get token {
+    if (_expirydate != null &&
+        _expirydate.isAfter(DateTime.now()) &&
+        _token != null) {
+      return _token;
+    }
+    return null;
+  }
+
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/$urlSegment?key=AIzaSyCJpTXC9fl7BBwxxhCU7acy9o_w5v37z1U ';
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+      _token = responseData['idToken'];
+      _userId = responseData['localId'];
+      _expirydate = DateTime.now()
+          .add(Duration(seconds: int.parse(responseData['expiresIn'])));
+      notifyListeners();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'accounts:signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'accounts:signInWithPassword');
+  }
+}
